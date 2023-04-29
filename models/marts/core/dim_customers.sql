ad19dec60fcdc55a6678f1f8bf8a93915b7b7602
priyaranjansnowflake@gmail.com <priyaranjansnowflake@gmail.com>
2023-04-29 11:43:43 +0000

    2nd commit

---
+++ models/marts/core/dim_customers.sql
@@ -0,0 +1,21 @@
+with customers as (
+    select * from {{ ref('stg_customer')}}
+),
+
+orders as (
+    select * from {{ ref('stg_orders')}}
+),
+
+customer_orders as (
+    select
+        customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+    
+    from orders
+
+    group by 1
+)
+
+select * from orders
